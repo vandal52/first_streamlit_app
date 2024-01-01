e479101b541b6f9d973be852d61c872894a538fa
vandal52 <155384181+vandal52@users.noreply.github.com>
2024-01-01 10:51:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥗Kaye, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
