b55fb10dd25e7a838f597710ba6efdee1637d3dd
vandal52 <155384181+vandal52@users.noreply.github.com>
2024-01-03 15:25:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,8 @@ try:
   else:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dtaframe(back_from_function)
-except URLError as e:
-  streamlist.error()
+#except URLError as e:
+#  streamlist.error()
 
 #debug
 streamlit.stop()
